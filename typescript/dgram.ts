053d40d0e881871f495b6747e00583b58e6c2df4
cosmostellar <160427240+cosmostellar@users.noreply.github.com>
2024-04-17 10:53:50 +0900

    Add a comment

--- typescript/dgram.ts
+++ typescript/dgram.ts
@@ -1,5 +1,10 @@
 import dgram from "dgram";
 
+/*
+Use the following command to try it yourself.
+nc -u 127.0.0.1 5500
+*/
+
 const socket = dgram.createSocket("udp4");
 
 socket.bind(5500, "127.0.0.1");
