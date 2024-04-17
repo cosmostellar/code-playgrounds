e1e6dc9c6f854275030a37027d6d76255e8ccb19
cosmostellar <160427240+cosmostellar@users.noreply.github.com>
2024-04-17 10:12:08 +0900

    Add more code

---
+++ typescript/dgram.ts
@@ -0,0 +1,10 @@
+import dgram from "dgram";
+
+const socket = dgram.createSocket("udp4");
+
+socket.bind(5500, "127.0.0.1");
+socket.on("message", (msg, info) => {
+    console.log(
+        `* My server got a datagram from ${info.address}:${info.port}\n: ${msg}`
+    );
+});
