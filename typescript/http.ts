420a4d38ddfceb7eb3d9f5f5472d990d50ea57ac
cosmostellar <160427240+cosmostellar@users.noreply.github.com>
2024-04-24 08:44:20 +0900

    Add more code

---
+++ typescript/http.ts
@@ -0,0 +1,45 @@
+import http from "http";
+import { AddressInfo } from "net";
+
+/*
+:: (IPv6) -> 0.0.0.0 (IPv4)
+Unspecified address
+*/
+
+const displayListeningMessage = (addressInfo: AddressInfo): void => {
+    if (addressInfo.family === "IPv6") {
+        console.log(
+            `Listening on http://[${addressInfo.address}]:${addressInfo.port} (${addressInfo.family})`
+        );
+    } else {
+        console.log(
+            `Listening on http://${addressInfo.address}:${addressInfo.port} (${addressInfo.family})`
+        );
+    }
+};
+
+// -----
+
+const httpServerIPv4 = http.createServer();
+httpServerIPv4.on("request", (req, res) => res.end("Hey there!"));
+httpServerIPv4.on("listening", () => {
+    const addressInfo = httpServerIPv4.address() as AddressInfo;
+    displayListeningMessage(addressInfo);
+});
+httpServerIPv4.on("error", (err) => {
+    console.log(`Error ${err}`);
+});
+httpServerIPv4.listen(0, "127.0.0.1");
+
+// -----
+
+const httpServerIPv6 = http.createServer();
+httpServerIPv6.on("request", (req, res) => res.end("Hey there!"));
+httpServerIPv6.on("listening", () => {
+    const addressInfo = httpServerIPv6.address() as AddressInfo;
+    displayListeningMessage(addressInfo);
+});
+httpServerIPv6.on("error", (err) => {
+    console.log(`Error ${err}`);
+});
+httpServerIPv6.listen(0, "::1");
